0158b9564b79d4eb755e165295e3b1a75527abe5
prrThr <arthur.op9@gmail.com>
2022-08-09 23:01:52 -0300

    getStates

--- src/services/secretary/SecretaryService.js
+++ src/services/secretary/SecretaryService.js
@@ -3,7 +3,11 @@ import GenericService from '../GenericService';
 const SecretaryService = (onAnyError) => {
     const generic = GenericService('secretary', 'secretaryUser', onAnyError);
 
-    return { ...generic, };
+    const getStates = () => {
+        return HttpHelper.get('secretary/state', generic.getUser().token).then(onAnyLog);
+    };
+
+    return { ...generic, getStates };
 };
 
 export default SecretaryService;
