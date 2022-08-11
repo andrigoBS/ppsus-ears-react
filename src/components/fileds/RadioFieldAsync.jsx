127436fd3dea8f523d915b6a2352b1a58ec65097
Andrigo Santos <andrigo.santos@luizalabs.com>
2022-08-11 04:37:39 -0300

    ajustes select e radio

---
+++ src/components/fileds/RadioFieldAsync.jsx
@@ -0,0 +1,23 @@
+import { CircularProgress } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import RadioField from './RadioField';
+
+const RadioFieldAsync = ({ getValue, watch, ...props }) => {
+    const [values, setValues] = useState(getValue? null : []);
+
+    useEffect(() => {
+        if(getValue) {
+            getValue().then(r => r.body).then(setValues);
+        }
+    }, [watch || undefined]);
+
+    if(!values){
+        return (<CircularProgress />);
+    }
+
+    return (
+        <RadioField values={values} {...props}/>
+    );
+};
+
+export default RadioFieldAsync;
