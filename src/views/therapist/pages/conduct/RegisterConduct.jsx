7dd86b71ad32b44fd420a719d3258cf873b222fd
KarolineGuckert <karolinesouzaguckert@gmail.com>
2022-08-15 20:25:53 -0300

    Adjusting fields

--- src/views/therapist/pages/conduct/RegisterConduct.jsx
+++ src/views/therapist/pages/conduct/RegisterConduct.jsx
@@ -18,17 +18,17 @@ const RegisterConduct = () => {
             <Grid item xs={12} sm={12} md={12}>
                 <TextField  {...register('accompanyDescription')} label="Descrição do acompanhamento" variant="outlined" size="small" multiline rows={4} required />
             </Grid>
-            <Grid item xs={12} sm={12} md={12}>
-                <SelectFieldAsync register={register('type')} title={'Tipo de TAN'} getValue={configuration.service.getTriageTypes}/>
-            </Grid>
             <Grid item xs={12} sm={12} md={6}>
                 <RadioField register={register('leftEar')} title={'Orelha Esquerda'} values={[{ id: true, name: 'Passou' },{ id: false, name: 'Falhou' }]} />
             </Grid>
             <Grid item xs={12} sm={12} md={6}>
                 <RadioField register={register('rightEar')} title={'Orelha Direita'} values={[{ id: true, name: 'Passou' },{ id: false, name: 'Falhou' }]} />
             </Grid>
-            <Grid item xs={12} sm={12} md={12}>
+            <Grid item xs={12} sm={12} md={6}>
                 <RadioField register={register('irda')} title={'IRDA'} values={[{ id: true, name: 'Possui' },{ id: false, name: 'Não Possui' }]} />
+            </Grid>
+            <Grid item xs={12} sm={12} md={6}>
+                <RadioField register={register('testType')} title={'Tipo de teste'} values={[{ id: 1, name: 'Teste' },{ id: 2, name: 'Reteste' },{ id: 3, name: 'Teste e reteste' }]} />
             </Grid>
         </BaseRegisterPaper>
     );
